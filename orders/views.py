6458377ccab6b7d369e8196f51f0b115d0e378e8
Wagonowod <kyzya95@bk.ru>
2023-02-03 20:07:34 +0500

    Task_3. Create chekup robot serial

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your views here.
+from robots.models import Robot
+from orders.models import Order
+
+
+@receiver(post_save, sender=Robot)
+def robot_created(sender, instance, **kwargs):
+    order = Order.objects.filter(robot_serial=instance.serial)
+    message = f"""Добрый день!\nНедавно вы интересовались нашим роботом модели '{instance.model}', версии '{instance.version}'.\nЭтот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами"""
+    if order:
+        print(message)
