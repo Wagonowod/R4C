002fa358b134d8ac25e1434d885da62a3566e82c
Wagonowod <kyzya95@bk.ru>
2023-02-06 15:03:57 +0500

    Task_2 Сhanging sql queries to django orm

--- robots/views.py
+++ robots/views.py
@@ -1,32 +1,36 @@
-from rest_framework.generics import ListAPIView
+from openpyxl.styles import Alignment
 from openpyxl.workbook import Workbook
-from django.http import HttpResponse
 from openpyxl.writer.excel import save_virtual_workbook
-import sqlite3
+from rest_framework.generics import ListAPIView
+from django.http import HttpResponse
+from django.db.models import Count, Q
+from datetime import datetime, timedelta
+
+from robots.models import Robot
 
 
 class ReportViewSet(ListAPIView):
 
     def get(self, request, format=None):
-
+        delta = datetime.now() - timedelta(weeks=1)
+        delta.strftime("%Y-%m-%d %H:%M:%S")
         wb = Workbook()
-        with sqlite3.connect('db.sqlite3') as db:
-            cursor = db.cursor()
-            db_select_query = """SELECT DISTINCT model FROM robots_robot"""
-            cursor.execute(db_select_query)
-            records = cursor.fetchall()
-            for row in records:
-                cursor.execute(
-                    f"SELECT model, version, COUNT(created) FROM robots_robot WHERE model = '{row[0]}' Group by version")
-                rec = cursor.fetchall()
-                ws = wb.create_sheet(row[0])
-                ws.title = row[0]
-                ws['A1'] = 'Модель'
-                ws['B1'] = 'Версия'
-                ws['C1'] = 'Количество за неделю'
-                #todo поправить ширину колонки С
-                for data in rec:
-                    ws.append(data)
+        records = Robot.objects.values('model').distinct()
+        for row in records.all():
+            rec = Robot.objects \
+                .filter(Q(model=row['model']) & Q(created__gt=delta)) \
+                .values('model', 'version') \
+                .annotate(total=Count('version'))
+            ws = wb.create_sheet(row['model'])
+            ws['A1'] = 'Модель'
+            ws['B1'] = 'Версия'
+            ws['C1'] = 'Количество за неделю'
+            ws.column_dimensions['C'].width = 21
+            for data in rec:
+                ws.append(list(data.values()))
+            for item in ws.iter_rows(min_row=1, max_row=ws.max_row, max_col=3):
+                for cell in item:
+                    cell.alignment = Alignment(horizontal='center')
         std = wb.get_sheet_by_name('Sheet')
         wb.remove_sheet(std)
         response = HttpResponse(content=save_virtual_workbook(wb), content_type='application/ms-excel')
